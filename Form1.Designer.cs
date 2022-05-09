a0c680aff39ba9e1272684a3d8ebe382a9876234
Alexandru Robert Lazar <alex2000robert@yahoo.com>
2022-05-09 16:35:01 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace TestsTFTForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testNumbersButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonInputFile = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.errorsLabel = new System.Windows.Forms.Label();
+            this.testsProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // testNumbersButton
+            // 
+            this.testNumbersButton.Enabled = false;
+            this.testNumbersButton.Location = new System.Drawing.Point(123, 176);
+            this.testNumbersButton.Name = "testNumbersButton";
+            this.testNumbersButton.Size = new System.Drawing.Size(134, 23);
+            this.testNumbersButton.TabIndex = 0;
+            this.testNumbersButton.Text = "Realizar pruebas";
+            this.testNumbersButton.UseVisualStyleBackColor = true;
+            this.testNumbersButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Enabled = false;
+            this.folderTextBox.Location = new System.Drawing.Point(43, 75);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(214, 23);
+            this.folderTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Carpeta con fichero de entrada:";
+            // 
+            // buttonInputFile
+            // 
+            this.buttonInputFile.Location = new System.Drawing.Point(278, 75);
+            this.buttonInputFile.Name = "buttonInputFile";
+            this.buttonInputFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonInputFile.TabIndex = 5;
+            this.buttonInputFile.Text = "Elegir";
+            this.buttonInputFile.UseVisualStyleBackColor = true;
+            this.buttonInputFile.Click += new System.EventHandler(this.buttonInputFile_Click);
+            // 
+            // errorsLabel
+            // 
+            this.errorsLabel.AutoSize = true;
+            this.errorsLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorsLabel.Location = new System.Drawing.Point(43, 111);
+            this.errorsLabel.Name = "errorsLabel";
+            this.errorsLabel.Size = new System.Drawing.Size(0, 15);
+            this.errorsLabel.TabIndex = 6;
+            // 
+            // testsProgressBar
+            // 
+            this.testsProgressBar.Location = new System.Drawing.Point(43, 136);
+            this.testsProgressBar.Name = "testsProgressBar";
+            this.testsProgressBar.Size = new System.Drawing.Size(310, 23);
+            this.testsProgressBar.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 211);
+            this.Controls.Add(this.testsProgressBar);
+            this.Controls.Add(this.errorsLabel);
+            this.Controls.Add(this.buttonInputFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.folderTextBox);
+            this.Controls.Add(this.testNumbersButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testNumbersButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox folderTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonInputFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label errorsLabel;
+        private System.Windows.Forms.ProgressBar testsProgressBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using TFTService;
+
+namespace TestsTFTForms
+{
+    public partial class Form1 : Form
+    {
+        private static string baseDirectory = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /**
+         * <summary>Botón para ejecutar las pruebas.</summary>
+         */
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            testNumbersButton.Enabled = false;
+            MainServiceClient serviceClient = new MainServiceClient();
+            if (baseDirectory == "")
+            {
+                errorsLabel.Text = "Error: No se ha seleccionado ninguna carpeta.";
+                return;
+            }
+            StreamReader srInput = null;
+            StreamWriter srOutput = null;
+            try
+            {
+                srInput = new StreamReader(baseDirectory + @"\entrada.txt");
+                srOutput = new StreamWriter(baseDirectory + @"\salida.txt", false, Encoding.UTF8);
+                string[] numbers = srInput.ReadToEnd().Split("\r\n");
+                testsProgressBar.Value = 0;
+                testsProgressBar.Maximum = numbers.Length;
+                foreach (string number in numbers)
+                {
+                    testsProgressBar.Increment(1);
+                    if (!number.Contains(":")) break;
+                    string[] numberForms = number.Split(":");
+                    string resp = await serviceClient.ComputeNumberAsync(numberForms[1]);
+                    if (resp != numberForms[0])
+                    {
+                        srOutput.WriteLine("Caso " + numberForms[1] + " ha fallado. Esperado: " + numberForms[0] + "; Actual: " + resp);
+                    }
+                }
+                testNumbersButton.Enabled = true;
+                srInput.Close();
+                srOutput.Close();
+            } catch (Exception ex)
+            {
+                errorsLabel.Text = ex.Message;
+                if (srInput != null) srInput.Close();
+                if (srOutput != null) srOutput.Close();
+                testNumbersButton.Enabled = true;
+            }       
+            
+        }
+        /**
+         * <summary>Botón para elegir fichero de entrada.</summary>
+         */
+        private void buttonInputFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                baseDirectory = folderBrowserDialog1.SelectedPath;
+                folderTextBox.Text = baseDirectory;
+                if (baseDirectory != "")
+                {
+                    testNumbersButton.Enabled = true;
+                } else
+                {
+                    testNumbersButton.Enabled = false;
+                }
+                
+            }
+        }
+    }
+}
